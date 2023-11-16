18575be5fe9ac5dbd314005de373b5820d79fedc
6cryptic <marouanedm98@gmail.com>
2023-11-16 03:40:32 -0800

    done

--- 0x12-singly_linked_lists/101-hello_school.asm
+++ 0x12-singly_linked_lists/101-hello_school.asm
@@ -1,15 +1,16 @@
-	SECTION .data
-msg:	db "Hello, Holberton", 0
-fmt:	db "%s", 10, 0
+SECTION .data
+    msg db "Hello, Holberton", 0    ; Null-terminated string
+    fmt db "%s", 10, 0              ; Format string: "%s\n"
 
-	SECTION .text
-	extern printf
-	global main
+SECTION .text
+    extern printf
+    global main
+
 main:
-	mov esi, msg
-	mov edi, fmt
-	mov eax, 0
-	call printf
+    mov rdi, fmt      ; Load the format string address into rdi register
+    mov rsi, msg      ; Load the message address into rsi register
+    xor rax, rax      ; Clear RAX register (used for the printf return value)
+    call printf       ; Call printf function to print the string
 
-	mov eax, 0
-	ret
+    xor rax, rax      ; Set return value to 0 (exit status)
+    ret               ; Return from the main function
